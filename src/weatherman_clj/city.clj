6d4ede056e1b71056e5150b78698f194bfab1198
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2015-12-17 13:58:41 +0100

    better matching

--- src/weatherman_clj/city.clj
+++ src/weatherman_clj/city.clj
@@ -2,12 +2,11 @@
 
 (import '(java.io BufferedReader FileReader))
 
-(def all-lines (atom []))
-
 (defn find-matching-cities [city-name]
-  (reset! all-lines [])
-  (with-open [rdr (java.io.BufferedReader.
-                   (java.io.FileReader. "resources/city_list.json"))]
-    (doseq [seq (line-seq rdr)]
-      (swap! all-lines (fn [current] (conj current seq)))))
-  (filter (fn [line] (re-find (re-pattern city-name) line)) @all-lines))
+  (let [matches (atom [])]
+    (with-open [rdr (java.io.BufferedReader.
+                     (java.io.FileReader. "resources/city_list.json"))]
+      (doseq [seq (line-seq rdr)]
+        (if (re-find (re-pattern city-name) seq)
+          (swap! matches (fn [current] (conj current seq))))))
+    @matches))
